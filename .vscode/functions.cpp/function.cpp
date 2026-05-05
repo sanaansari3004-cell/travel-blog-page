d03ae0f0e96848a418d2dd516c1622cdcff8bcea
Sania Ansari <sanaansari3004@gmail.com>
2026-05-06 00:40:42 +0530

    first commit

---
+++ .vscode/functions.cpp/function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int product(int a,int b){
+int p=  a*b;
+return p;
+}
+int main(){
+    int product(2,4);
+    return 0; 
+}
---
+++ link generator/script.js
@@ -0,0 +1,4 @@
+function createLink() {
+    document.getElementById("demo").innerHTML =
+        '<a href="https://www.google.com" target="_blank">Open Google</a>';
+}
